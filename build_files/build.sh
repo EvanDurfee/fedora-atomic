4eccd60bb001bdfd9ee11254916cc02da3c4e654
Evan Durfee <edurfee@pm.me>
2025-11-01 22:21:28 +0100

    remove dnf update from protonvpn install

--- build_files/build.sh
+++ build_files/build.sh
@@ -124,7 +124,6 @@ dnf5 -y copr disable ublue-os/packages
 wget "https://repo.protonvpn.com/fedora-$(cat /etc/fedora-release | cut -d' ' -f 3)-stable/protonvpn-stable-release/protonvpn-stable-release-1.0.3-1.noarch.rpm"
 dnf5 install -y ./protonvpn-stable-release-1.0.3-1.noarch.rpm
 rm ./protonvpn-stable-release-1.0.3-1.noarch.rpm
-dnf5 check-update --refresh
 dnf5 install -y proton-vpn-gnome-desktop
 
 just --justfile=/ctx/distro-utils/distrobox-auto/justfile install
