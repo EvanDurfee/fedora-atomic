82cc4d9952c39d6a936612591fc09a1107824071
Evan Durfee <edurfee@pm.me>
2025-11-02 10:39:25 +0100

    Remove uupd related section from post-install
    Removes the commands to enable uupd and/or the base flatpak and
    rpmostreed update timers from the post-install step, as these are now
    included in the upstream base image.

--- build_files/post_install.sh
+++ build_files/post_install.sh
@@ -2,16 +2,6 @@
 
 set -ouex pipefail
 
-# Bluefin update timer
-if test -e /usr/lib/systemd/system/uupd.timer; then
-    systemctl enable uupd.timer
-else
-    systemctl enable rpm-ostreed-automatic.timer
-    systemctl enable flatpak-system-update.timer
-    systemctl --global enable flatpak-user-update.timer
-fi
-
-
 # Enable system flatpak installs
 systemctl enable flatpak-installation-sync.service
 systemctl --global enable flatpak-installation-sync.service
